9e380210a03b69f7b0e4f2fcf1f8e0a689202c32
Damian <damian.vlaicu@gmail.com>
2026-06-29 22:59:30 +0200

    removed unnecessary fonts and removed unused packages

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,27 +3,13 @@ import {Stack} from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import {StatusBar} from "expo-status-bar";
 
-import {useFonts, BricolageGrotesque_700Bold} from "@expo-google-fonts/bricolage-grotesque";
-import {
-    PlusJakartaSans_400Regular,
-    PlusJakartaSans_500Medium,
-    PlusJakartaSans_600SemiBold
-} from "@expo-google-fonts/plus-jakarta-sans";
-import {SpaceMono_400Regular} from "@expo-google-fonts/space-mono";
-
 import "../global.css";
 import {useAuthStore} from "@/store/useAuthStore";
 
 void SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-    const [fontsLoaded, fontError] = useFonts({
-        BricolageGrotesque_700Bold,
-        PlusJakartaSans_400Regular,
-        PlusJakartaSans_500Medium,
-        PlusJakartaSans_600SemiBold,
-        SpaceMono_400Regular,
-    });
+
     const {isInitialized, initAuth} = useAuthStore();
 
     useEffect(() => {
@@ -31,14 +17,12 @@ export default function RootLayout() {
     }, []);
 
     useEffect(() => {
-        const assetsReady = fontsLoaded || fontError;
 
-        if (assetsReady && isInitialized) {
+        if (isInitialized) {
             void SplashScreen.hideAsync();
         }
-    }, [fontsLoaded, fontError, isInitialized]);
+    }, [isInitialized]);
 
-    if (!fontsLoaded || !isInitialized) return null;
 
     return (
         <>
